85ec65329a87619cfdc3bd0a31e484f9473e8345
solegaonkar <vikas.solegaonkar@gmail.com>
2019-06-20 10:16:21 +0530

    Update

---
+++ commit.bat
@@ -0,0 +1,8 @@
+git config --global user.name "solegaonkar"
+git config --global user.email "vikas.solegaonkar@gmail.com"
+git config --global http.proxy http://tmsharda2.techmahindra.com:8080
+
+git add .
+git commit -m "Update"
+git push
+
