ba2c46422c319483c09bf1a3524e866ae1d0c642
solegaonkar <vikas.solegaonkar>
2019-07-16 21:22:08 +0530

    Fix to remove old G tags

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,37 +1,12 @@
-const head = '<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"><div id="header" class="container"><img src="img/thewiz.png" class="image-anchor" onclick="window.location = \'index.html\'" width="15%" ><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="navbarResponsive"><ul class="navbar-nav ml-auto"><li class="nav-item"><a class="nav-link link" href="UsefulResources.html">Resources</a></li><li class="nav-item"><a class="nav-link link" href="UsefulBooks.html">Books</a></li><li class="nav-item"><a class="nav-link link" href="UsefulResearchPapers.html">Research</a></li></ul></div></div></nav><div class="container"><div class="row">&nbsp;</div><div class="row">&nbsp;</div><div class="row"><div class="col-lg-2"></div><div class="col-lg-8"><div class="container"><div class="row"><div class="col-12"></div></div><div class="row"><div class="col-12">';
-const tail = '<gcse:search></gcse:search></div></div></div></div></div></div><div></div><footer class="py-5"><div class="container"><p class="m-0 text-center text-muted">Copyright &copy; Vikas K. Solegaonkar 2019</p></div></footer>';
+const head = '<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"><div id="header" class="container"><img src="img/thewiz.png" class="image-anchor" onclick="window.location = \'index.html\'" ><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="navbarResponsive"><ul class="navbar-nav ml-auto"><li class="nav-item"><a class="nav-link link" href="UsefulResources.html">Resources</a></li><li class="nav-item"><a class="nav-link link" href="UsefulBooks.html">Books</a></li><li class="nav-item"><a class="nav-link link" href="UsefulResearchPapers.html">Research</a></li></ul></div></div></nav><div class="container"><div class="row">&nbsp;</div><div class="row">&nbsp;</div><div class="row"><div class="col-lg-2"></div><div class="col-lg-8"><div class="container"><div class="row"><div class="col-12"></div></div><div class="row"><div class="col-12">';
+// const tail = '<gcse:search></gcse:search></div></div></div></div></div></div><div></div><footer class="py-5"><div class="container"><p class="m-0 text-center text-muted">Copyright &copy; Vikas K. Solegaonkar 2019</p></div></footer>';
+const tail = '</div></div></div></div></div></div><div></div><footer class="py-5"><div class="container"><p class="m-0 text-center text-muted">Copyright &copy; Vikas K. Solegaonkar 2019</p></div></footer>';
 var body = $("#body").html();
 $("#body").html(head + body + tail);
 
 $('pre code').each(function (i, block) {
     hljs.highlightBlock(block);
 });
-
-(function () {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = 'https://www.googletagmanager.com/gtag/js?id=UA-115049689-5';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
-
-(function () {
-    var cx = '001416028576861424674:idwrcpies6m';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-})();
-
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-    dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'UA-115049689-5');
 
 var blogList;
 
